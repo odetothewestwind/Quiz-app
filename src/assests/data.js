91869ef3e24ca9a4170a2ff0aafdc8c6842493ec
Abdul Karim <abdulkarim4696@gmail.com>
2024-10-09 19:49:50 +0530

    first commit

---
+++ src/assests/data.js
@@ -0,0 +1,642 @@
+export const data = [
+    {
+        question: "which device is required for internet connection?",
+        option1: "Modem",
+        option2: "Route",
+        option3: "Lan Cable",
+        option4: "Pen Drive",
+        ans: 1,
+
+    },
+
+    {
+        question: "which is the largest state in india?",
+        option1: "Rajasthan",
+        option2: "MP",
+        option3: "Orissa",
+        option4: "Maharastra",
+        ans: 1
+
+    },
+
+    {
+        question: "which is the following is national bird od india ?",
+        option1: "sparrow",
+        option2: "Nightangle",
+        option3: "peacock",
+        option4: "kiwi",
+        ans: 3
+
+    },
+
+
+    {
+        question: "who is the first prime minister of India?",
+        option1: "sardar patel",
+        option2: "Pt Nehrul",
+        option3: "Indira Gandhi ",
+        option4: "Br Ambedkar",
+        ans: 2
+
+    },
+
+    {
+        question: "which is the highest peak of the world?",
+        option1: "k2 peak",
+        option2: "sir john's",
+        option3: "Mount Everest",
+        option4: "Nanda Devi",
+        ans: 3
+
+    },
+
+    {
+       question: " In what year did the Great October Socialist Revolution take place?",
+
+option1: 1917,
+option2: 1923,
+option3: 1914,
+option4: 1920,
+ Ans: 1
+    },
+
+
+    {
+        question: " What is the largest lake in the world?",
+ 
+        option1:  'Caspian',
+        option2:  'Baikal',
+        option3:  'Superior',
+        option4:  'Ontario',
+  Ans: 2
+     },
+
+
+     {
+        question: " Which planet in the solar system is known as the “Red Planet?",
+ 
+        option1:  'venus',
+        option2:  'earth',
+        option3:  'mars',
+        option4:  'jupitar',
+  Ans: 3
+     },
+
+     {
+        question: " Who wrote the novel “War and Peace?",
+ 
+        option1:  'Anton',
+        option2:  'Fyodor' ,
+        option3:  'Leo Tolstoy',
+        option4:  'Ivan Turgenev',
+  Ans: 3
+     },
+
+
+     {
+        question: " What is the capital of Japan?",
+ 
+        option1:  'Beijing',
+        option2:  'Tokyo' ,
+        option3:  'Seoul',
+        option4:  'Bangkok',
+  Ans: 2
+     },
+
+     {
+        question: " Which river is the longest in the world?",
+ 
+        option1:  'Amazon',
+        option2:  'Mississippi' ,
+        option3:  'Nile',
+        option4:  'Yangtze',
+  Ans: 3
+     },
+     
+
+     {
+        question: " What gas is used to extinguish fires?",
+ 
+        option1:  'Oxygen',
+        option2:  'Nitrogen' ,
+        option3:  'Carbon dioxide',
+        option4:  'Hydrogen',
+  Ans: 3
+     },
+
+
+     {
+        question: "  What animal is the national symbol of Australia?",
+ 
+        option1:  'Kangaroo',
+        option2:  'Koala' ,
+        option3:  'Emu',
+        option4:  'Hydrogen',
+  Ans: 3
+     },
+
+
+     {
+        question: "  Which of the following planets is not a gas giant?",
+ 
+        option1:  'Mars',
+        option2:  'Jupiter' ,
+        option3:  'Saturn',
+        option4:  'Uranus',
+  Ans: 1
+     },
+
+
+     {
+        question: "   What is the name of the process by which plants convert sunlight into energy?",
+ 
+        option1:  'Respiration',
+        option2:  'Photosynthesis' ,
+        option3:  'Oxidation',
+        option4:  'Evolution',
+  Ans: 2
+     },
+
+     {
+        question: "   What chemical element is designated as “Hg”?",
+ 
+        option1:  'Silver',
+        option2:  'tin' ,
+        option3:  'Copper',
+        option4:  'Mercurary',
+  Ans: 4
+     },
+
+
+     {
+        question: "    In what year was the first international modern Olympiad held?",
+ 
+        option1: 1896,
+        
+        option2: 1900,
+        option3: 1912,
+        option4: 1924, 
+  Ans: 1
+     },
+
+
+     {
+        question: " For which of these disciplines Nobel Prize is awarded?",
+ 
+        option1: 'Physics, Chemistry',
+        
+        option2: 'Physiology',
+        option3: 'Medicine',
+        option4: 'All of the above', 
+  Ans: 4
+     },
+
+     {
+        question: " Entomology is the science that studies:",
+ 
+        option1: 'Behavior of human beings',
+        
+        option2: 'Insects',
+        option3: 'The origin and history of technical and scientific terms',
+        option4: 'The formation of rocks', 
+  Ans: 2
+     },
+
+
+     
+     {
+        question: " Hitler's party is known as:",
+ 
+        option1: 'Labour Party',
+        
+        option2: 'Nazi Party',
+        option3: 'Ku-Klux-Klan',
+        option4: 'Democratic Party', 
+  Ans: 2
+     },
+
+
+     
+     {
+        question: " Hitler's party is known as:",
+ 
+        option1: 'Labour Party',
+        
+        option2: 'Nazi Party',
+        option3: 'Ku-Klux-Klan',
+        option4: 'Democratic Party', 
+  Ans: 2
+     },
+
+     {
+        question: " . For which Galileo is famous",
+ 
+        option1: 'Developed the telescope',
+        
+        option2: 'Discovered four satellites of Jupiter',
+        option3: 'Found that the swinging motion of the pendulum results in consistent time measurement.',
+        option4: 'All of the above', 
+  Ans: 4
+     },
+
+     {
+        question: " When the First Afghan War took place in",
+ 
+        option1: 1839,
+        
+        option2: 1843,
+        option3: 1833,
+        option4: 1848, 
+  Ans: 1
+     },
+
+     
+     {
+        question: " Ecology deals with",
+ 
+        option1: 'Birds',
+        
+        option2: 'cell formation',
+        option3: 'Relation between organisms and their environment',
+        option4: 'Tissues', 
+  Ans: 3
+     },
+
+     {
+        question: " Which is the largest island",
+ 
+        option1: 'New Guinea',
+        
+        option2: 'Andaman Nicobar',
+        option3: 'Greenland',
+        option4: 'Hawaii', 
+  Ans: 3
+     },
+
+     {
+        question: "Which one is the hottest continent",
+ 
+        option1: 'Africa',
+        
+        option2: 'South Asia',
+        option3: 'North America',
+        option4: 'Australia', 
+  Ans: 1
+     },
+
+
+     {
+        question: "What do Koalas usually eat",
+ 
+        option1: 'Bamboo',
+        
+        option2: 'Eucalyptus',
+        option3: 'Aloe Vera',
+        option4: 'Banana', 
+  Ans: 2
+     },
+
+
+     
+     {
+        question: "What is the most popular bread in France",
+ 
+        option1: 'Brioche',
+        
+        option2: 'Baguette',
+        option3: 'White bread',
+        option4: 'Ciabatta', 
+  Ans: 2
+     },
+
+     {
+        question: "What is the official currency of Japan",
+ 
+        option1: 'Won',
+        
+        option2: 'yuan',
+        option3: 'yen',
+        option4: 'Dollar', 
+  Ans: 3
+     },
+
+     {
+
+       question: 'What is the phobia of thunder and rain?',
+
+option1: 'Agoraphobia',
+option2: 'Ombrophobia',
+option3:' Acrophobia',
+option4: 'Claustrophobia',
+Ans: 2
+     },
+
+
+     {
+        question: "What does Carpe Diem mean in Latin",
+ 
+        option1: 'Enjoy the moment',
+        
+        option2: 'Have no fear',
+        option3: 'Sorry I blew it',
+        option4: 'Hello', 
+  Ans: 1
+     },
+
+     {
+        question: "Which one of the following countries is not in Africa",
+ 
+        option1: 'Morocco',
+        
+        option2: 'Yemen',
+        option3: 'sudan',
+        option4: 'Algeria', 
+  Ans: 2
+     },
+
+     
+     {
+        question: "What is considered the lung of the Earth",
+ 
+        option1: 'Amazon rainforest',
+        
+        option2: 'The Mississippi River',
+        option3: 'The Sahara',
+        option4: 'Everest', 
+  Ans: 1
+     },
+     
+     
+     {
+        question: "Where does the Sushi come from",
+ 
+        option1: 'japan',
+        
+        option2: 'korea',
+        option3: 'America',
+        option4: 'Brazil', 
+  Ans: 1
+     },
+     
+     
+     {
+        question: " In which century the Mona Lisa was painted",
+ 
+        option1: '18th century',
+        
+        option2: '15th century',
+        option3: '16th century',
+        option4: '14th century', 
+  Ans: 3
+     },
+     
+     
+     {
+        question: "Who wrote the “Great Gatsby” novel",
+ 
+        option1: 'Leo Tolstoy',
+        
+        option2: 'Hemingway',
+        option3: 'Stephen King',
+        option4: 'F. Scott Fitzgerald', 
+  Ans: 4
+     },
+     
+     
+     {
+        question: "Which is the richest country in the world",
+ 
+        option1: 'Qatar',
+        
+        option2: 'Russia',
+        option3: 'UAE',
+        option4: 'usa', 
+  Ans: 1
+     },
+     {
+        question: "Which county is the biggest grower of coffee",
+ 
+        option1: 'Spain',
+        
+        option2: 'Russia',
+        option3: 'UAE',
+        option4: 'Brazil', 
+  Ans: 4
+     },
+     {
+        question: "How many bones are in the body of an adult human",
+ 
+        option1: 330,
+        
+        option2: 206,
+        option3: 208,
+        option4: 306, 
+  Ans: 2
+     },
+     {
+        question: "When the humans use more facial muscles",
+ 
+        option1: 'While smiling',
+        
+        option2: 'While frowning',
+        option3: 'While sleeping',
+        option4: 'While talking', 
+  Ans: 2
+     },
+     {
+        question: "Who wrote “Crime and Punishment",
+ 
+        option1: 'Leo Tolstoy',
+        
+        option2: 'Fyodor Dostoevsky',
+        option3: 'Anton Chekhov',
+        option4: 'Ivan Turgenev', 
+  Ans: 2
+     },
+     {
+        question: " In what year was the United Nations (UN) founded",
+ 
+        option1: '1945',
+        
+        option2: '1919',
+        option3: '1947',
+        option4: '1942', 
+  Ans: 1
+     },
+     {
+        question: "Which city is called the “City of Winds",
+ 
+        option1: 'Chicago',
+        
+        option2: 'seattle',
+        option3: 'washington',
+        option4: 'virginia', 
+  Ans: 1
+     },
+     {
+        question: "What animal is a symbol of peace and neutrality",
+ 
+        option1: 'polar bear',
+        
+        option2: 'white lion',
+        option3: 'white tiger',
+        option4: 'white crane', 
+  Ans: 4
+     },
+     {
+        question: "In what year was Ferdinand Magellan's first successful circumnavigation of the world?",
+ 
+        option1: 1492,
+        option2: 1520,
+        option3: 1588,
+        option4:  1620,
+  Ans: 2
+     },
+     {
+        question: " What element is the main constituent of diamonds",
+ 
+        option1: 'carbon',
+        
+        option2: 'oxygen',
+        option3: 'silver',
+        option4: 'gold', 
+  Ans: 1
+     },
+     {
+        question: "What year was the first man sent to space",
+ 
+        option1: 1957,
+        option2: 1961,
+        option3: 1969,
+        option4: 1975, 
+  Ans: 2
+     },
+     {
+        question: "Which sea is considered the most salty on Earth",
+ 
+        option1: 'Red Sea',
+        option2: ' Mediterranean Sea',
+        option3: 'Dead sea', 
+        option4: 'Black Sea', 
+  Ans: 3
+     },
+     {
+        question: "Which organ in the human body is responsible for the secretion of bile",
+ 
+        option1: 'Liver',
+        option2: 'Kidneys',
+        option3: 'spleen',
+        option4: 'Stomach', 
+  Ans: 1
+     },
+     {
+        question: "Which planet in the solar system is known as the “Morning Star” or “Evening Star”?",
+ 
+        option1:  'Mars',
+        option2:  'Venus',
+        option3: 'Mercury',
+        option4: 'Jupiter', 
+  Ans: 2
+     },
+     {
+        question: "What chemical element is designated as “Cu”",
+ 
+        option1: 'Zinc',
+        option2: 'Copper',
+        option3: 'Cobalt',
+        option4: 'Kurtz', 
+  Ans: 2
+     },
+     {
+        question: "In what year did the French Revolution take place",
+ 
+        option1: 1789,
+        option2:  1812,
+        option3: 1905,
+        option4:  1793, 
+  Ans: 1
+     },
+     {
+        question: " What ocean is between Africa and Australia",
+ 
+        option1: 'Pacific',
+        option2: 'Indian',
+        option3: ' Atlantic',
+        option4: 'Arctic', 
+  Ans: 2
+     },
+     {
+        question: "Which chemical element makes up most of the atmosphere of Mars",
+ 
+        option1: 'Carbon dioxide', 
+        option2: 'Oxygen',
+        option3: '  Nitrogen',
+        option4: ' Hydrogen', 
+  Ans: 1
+     },
+     {
+        question: "What chemical element makes up most of the atmosphere of Titan",
+ 
+        option1: 'Methane',
+        option2: ' Carbon dioxide',
+        option3: 'Hydrogen',
+        option4: 'Oxygen', 
+  Ans: 1
+     },
+     {
+        question: " In what year did the discovery of Antarctica occur",
+ 
+        option1: 'Qatar',
+        
+        option2: 'Russia',
+        option3: 'UAE',
+        option4: 'usa', 
+  Ans: 1
+     },
+     {
+        question: " In what year was the International Red Cross and Red Guard Organization (Red Cross) founded?",
+ 
+        option1: 1500,
+        option2: 1778,
+        option3: 1820,
+        option4:1892, 
+  Ans: 3
+     },
+     {
+        question: "Which is the richest country in the world",
+ 
+        option1: 1863,
+        option2:  1901,
+        option3: 1945,
+        option4: 1980, 
+  Ans: 1
+     },
+     {
+        question: "What instrument is depicted in Leonardo da Vinci's famous painting “The Last Supper",
+ 
+        option1: 'Tablets',
+        option2: 'Lute',
+        option3: 'Guitar',
+        option4: 'Harp', 
+  Ans: 2
+     },
+     {
+        question: "In which country did the Chernobyl nuclear disaster take place?",
+ 
+        option1: 'Russia',
+        option2: 'Ukraine',
+        option3: 'Belarus',
+        option4: 'Lithuania', 
+  Ans: 2
+     },
+     {
+        question: "What is the name of the science that studies the past of life on Earth?",
+ 
+        option1: 'Palaeontology',
+        option2: 'Astronomy',
+        option3: 'Anthropology',
+        option4: 'Geology', 
+  Ans: 1
+     },
+     
+]
